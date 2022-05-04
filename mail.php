af434a03296fb02e4769a4958c9081029edff761
Vladislav Kushch <kushch.vladislav@gmail.com>
2022-05-04 19:27:06 +0300

    format

--- mail.php
+++ mail.php
@@ -3,6 +3,7 @@ $name = trim($_POST['contact-name']);
 $phone = trim($_POST['contact-phone']);
 $email = trim($_POST['contact-email']);
 $message = trim($_POST['contact-message']);
+
 if ($name == "") {
     $msg['err'] = "\n Name can not be empty!";
     $msg['field'] = "contact-name";
@@ -28,8 +29,10 @@ if ($name == "") {
     $msg['field'] = "contact-message";
     $msg['code'] = FALSE;
 } else {
+    // Email
     $to = 'nipabali@gmail.com';
     $subject = 'Doob Contact Query';
+
     $_message = '<html><head></head><body>';
     $_message .= '<p>Name: ' . $name . '</p>';
     $_message .= '<p>Phone: ' . $phone . '</p>';
@@ -42,6 +45,7 @@ if ($name == "") {
     $headers .= 'From:  Doob <nipabali@gmail.com>' . "\r\n";
     $headers .= 'cc: nipabali@gmail.com' . "\r\n";
     $headers .= 'bcc: nipabali@gmail.com' . "\r\n";
+
     mail($to, $subject, $_message, $headers, '-f nipabali@gmail.com');
 
     $msg['success'] = "\n Email has been sent successfully.";
